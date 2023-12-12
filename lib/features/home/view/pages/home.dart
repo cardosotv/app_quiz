c681cc23f1afe42ca47142b83fca28b29e950b3c
Tiago Cardoso <cardoso.tv@gmail.com>
2023-12-13 00:15:30 +1000

    title score

--- lib/features/home/view/pages/home.dart
+++ lib/features/home/view/pages/home.dart
@@ -37,7 +37,7 @@ class _HomeState extends State<Home> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              userScore("Your Score: ", loggedUser, myWidht: maxWidth),
+              userScore("Score: ", loggedUser, myWidht: maxWidth),
             ],
           ),
           // Row(
